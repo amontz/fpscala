2f0854c308d4a4e633705482f07f8e88f54a5939
al <allen.montz@gmail.com>
2016-04-20 22:26:51 -0500

    add tail recursive version

--- 2/fib.scala
+++ 2/fib.scala
@@ -1,10 +1,18 @@
 /* 2.1 */
 
+/* 0 1 1 2 3 5 8 13
+     | start here
+ For n-th term, we do n-1 additions,
+ but we loop n times. so always return prev
+*/
+
 object Fib {
   def fib(n: Int): Int = {
+    def go(n: Int, cur: Int, prev: Int): Int = {
+      if (n == 1) prev
+      else go(n-1, cur + prev, cur)
+    }
     if (n <= 0) -1
-    else if (n == 1) 0
-    else if (n == 2) 1
-    else fib(n-1) + fib(n-2)
+    else go(n, 1, 0)
   }
 }
