c4dd91b914451de43a98a7fe0736e04fd276a680
al <allen.montz@gmail.com>
2016-04-21 20:36:42 -0500

    implement rest of 2

--- 2/ps2.scala
+++ 2/ps2.scala
@@ -1,12 +1,11 @@
-/* 2.1 */
+object ps2 {
 
-/* 0 1 1 2 3 5 8 13
-     | start here
- For n-th term, we do n-1 additions,
- but we loop n times. so always return prev
-*/
-
-object Fib {
+  /* 2.1 */
+  /* 0 1 1 2 3 5 8 13
+       | start here
+   For n-th term, we do n-1 additions,
+   but we loop n times. so always return prev
+   */
   def fib(n: Int): Int = {
     def go(n: Int, cur: Int, prev: Int): Int = {
       if (n == 1) prev
@@ -15,11 +14,8 @@ object Fib {
     if (n <= 0) -1
     else go(n, 1, 0)
   }
-}
 
-/* 2.2 */
-
-object isSorted {
+  /* 2.2 */
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean) : Boolean = {
     @annotation.tailrec
     def loop(n: Int): Boolean = {
@@ -29,5 +25,18 @@ object isSorted {
     }
     if (as.length == 0) true
     else loop(1)
+  }
+
+  /* 2.3 */
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
+    (a: A) => ((b: B) => f(a,b))
+
+  /* 2.4 */
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  /* 2.5 */
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
   }
 }
